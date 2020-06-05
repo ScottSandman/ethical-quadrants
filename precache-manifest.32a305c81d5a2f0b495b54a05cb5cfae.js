23861522ac2b1e71f1abf917c66bd2190b82bcc5
Scott Sandman <eightmenout19@gmail.com>
2020-06-05 15:01:23 -0400

    Updates

---
+++ precache-manifest.32a305c81d5a2f0b495b54a05cb5cfae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2dd20881ea1ddfac8b41002c0679ae9",
+    "url": "/ethical-quadrants/index.html"
+  },
+  {
+    "revision": "794624d8c5f957101f29",
+    "url": "/ethical-quadrants/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "543b88048fdb82fbf841",
+    "url": "/ethical-quadrants/static/js/2.e7fa4ecb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ethical-quadrants/static/js/2.e7fa4ecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "794624d8c5f957101f29",
+    "url": "/ethical-quadrants/static/js/main.ddc862ee.chunk.js"
+  },
+  {
+    "revision": "d0228a582a900b85f6dd",
+    "url": "/ethical-quadrants/static/js/runtime-main.a2a0ca76.js"
+  }
+]);
